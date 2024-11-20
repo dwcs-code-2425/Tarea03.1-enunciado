68538e8da6e9356088cb375b6078ee3e02122962
dwcs-code-2425 <dwcs.code.2425@gmail.com>
2024-11-20 17:02:14 +0100

    init commit

---
+++ conexion.php
@@ -0,0 +1,15 @@
+
+<?php
+
+
+
+/**
+ * Summary of getConnection
+ * Crea un objeto PDO
+ * @return PDO|null un objeto PDO si ha habido éxito creando la conexión, null en caso contrario
+ */
+function getConnection()
+{
+
+   
+}
